30548cf501f92c9f6eaa70c1a491fcb42c9f8e66
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-01 02:00:29 +0200

    Update my_bot_1.js

--- my_bot_1.js
+++ my_bot_1.js
@@ -3,4 +3,4 @@ const client = new Discord.Client()
 client.on('ready', () => {
     console.log("Connected as" + client.user.tag)
 })
-client.login("ODQ5MDU4OTU0NDcwMjI4MDMw.YLVpxQ.fAvNf9jlj0GXGLYrhi_nOQRBSdE")
+client.login("")
